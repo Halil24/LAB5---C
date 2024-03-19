e60f49b8190f4399fd50ac3caba0b4c14a139bf2
Halil Bagosi <halilbagosi24@icloud.com>
2024-03-19 08:46:01 +0100

    Initial Commit

---
+++ LAB5/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LAB5
+//
+//  Created by Halil Bagosi on 19.3.24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
